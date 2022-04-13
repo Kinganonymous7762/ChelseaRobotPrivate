687ff7bf2d85cc3c6fcdf142bed0aebd17d40000
Xanon <74370153+Kinganonymous7762@users.noreply.github.com>
2022-04-13 15:09:05 +0530

    Update chatstatus.py

--- ChelseaRobot/modules/helper_funcs/telethn/chatstatus.py
+++ ChelseaRobot/modules/helper_funcs/telethn/chatstatus.py
@@ -1,7 +1,7 @@
 from telethon.tl.types import ChannelParticipantsAdmins
 
-from AnkiVector import DRAGONS
-from AnkiVector.modules.helper_funcs.telethn import IMMUNE_USERS, telethn
+from ChelseaRobot import DRAGONS
+from ChelseaRobot.modules.helper_funcs.telethn import IMMUNE_USERS, telethn
 
 
 async def user_is_ban_protected(user_id: int, message):

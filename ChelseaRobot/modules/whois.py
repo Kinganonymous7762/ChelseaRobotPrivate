05ca8512429ea8aeb5c3a67961a3240ec3544320
Xanon <74370153+Kinganonymous7762@users.noreply.github.com>
2022-04-13 15:23:54 +0530

    Update whois.py

--- ChelseaRobot/modules/whois.py
+++ ChelseaRobot/modules/whois.py
@@ -4,7 +4,7 @@ from pyrogram import filters
 from pyrogram.errors import PeerIdInvalid
 from pyrogram.types import Message, User
 
-from AnkiVector import pbot
+from ChelseaRobot import pbot
 
 
 def ReplyCheck(message: Message):

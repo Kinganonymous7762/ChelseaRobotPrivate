031066381ff52ae5a862d084570f751a32c96085
Xanon <74370153+Kinganonymous7762@users.noreply.github.com>
2022-04-13 15:26:17 +0530

    Update wallpaper.py

--- ChelseaRobot/modules/wallpaper.py
+++ ChelseaRobot/modules/wallpaper.py
@@ -4,8 +4,8 @@ import requests as r
 from telegram import Update
 from telegram.ext import CallbackContext, run_async
 
-from AnkiVector import SUPPORT_CHAT, WALL_API, dispatcher
-from AnkiVector.modules.disable import DisableAbleCommandHandler
+from ChelseaRobot import SUPPORT_CHAT, WALL_API, dispatcher
+from ChelseaRobot.modules.disable import DisableAbleCommandHandler
 
 # Wallpapers module by @TheRealPhoenix using wall.alphacoders.com
 

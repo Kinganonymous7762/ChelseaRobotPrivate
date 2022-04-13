af9547adcd7165906b6727a74a6025c2837228f4
Xanon <74370153+Kinganonymous7762@users.noreply.github.com>
2022-04-13 16:09:00 +0530

    Update log_channel_sql.py

--- ChelseaRobot/modules/sql/log_channel_sql.py
+++ ChelseaRobot/modules/sql/log_channel_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import Column, String, distinct, func
 
-from AnkiVector.modules.sql import BASE, SESSION
+from ChelseaRobot.modules.sql import BASE, SESSION
 
 
 class GroupLogs(BASE):

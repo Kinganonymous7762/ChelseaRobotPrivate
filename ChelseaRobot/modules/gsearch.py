03d6f53d583dd38498ccd758209565eb38c1c93a
Xanon <74370153+Kinganonymous7762@users.noreply.github.com>
2022-04-13 15:46:54 +0530

    Update gsearch.py

--- ChelseaRobot/modules/gsearch.py
+++ ChelseaRobot/modules/gsearch.py
@@ -4,7 +4,7 @@ from telethon import *
 from telethon.tl import functions, types
 from telethon.tl.types import *
 
-from AnkiVector.events import register
+from ChelseaRobot.events import register
 
 
 async def is_register_admin(chat, user):

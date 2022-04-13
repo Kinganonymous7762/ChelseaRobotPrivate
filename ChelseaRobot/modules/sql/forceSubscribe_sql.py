67686cc3b7a12f0970a3d6cd1b882aae36ea2ed4
Xanon <74370153+Kinganonymous7762@users.noreply.github.com>
2022-04-13 16:08:10 +0530

    Update forceSubscribe_sql.py

--- ChelseaRobot/modules/sql/forceSubscribe_sql.py
+++ ChelseaRobot/modules/sql/forceSubscribe_sql.py
@@ -16,7 +16,7 @@
 
 from sqlalchemy import Column, Numeric, String
 
-from AnkiVector.modules.sql import BASE, SESSION
+from ChelseaRobot.modules.sql import BASE, SESSION
 
 
 class forceSubscribe(BASE):

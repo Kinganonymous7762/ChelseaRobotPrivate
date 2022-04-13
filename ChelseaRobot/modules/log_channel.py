5d16e32a2bdc400436489016f34a6e4aa93a5f87
Xanon <74370153+Kinganonymous7762@users.noreply.github.com>
2022-04-13 15:44:03 +0530

    Update log_channel.py

--- ChelseaRobot/modules/log_channel.py
+++ ChelseaRobot/modules/log_channel.py
@@ -3,7 +3,7 @@ from functools import wraps
 
 from telegram.ext import CallbackContext
 
-from AnkiVector.modules.helper_funcs.misc import is_module_loaded
+from ChelseaRobot.modules.helper_funcs.misc import is_module_loaded
 
 FILENAME = __name__.rsplit(".", 1)[-1]
 
@@ -13,9 +13,9 @@ if is_module_loaded(FILENAME):
     from telegram.ext import CommandHandler, JobQueue, run_async
     from telegram.utils.helpers import escape_markdown
 
-    from AnkiVector import EVENT_LOGS, LOGGER, dispatcher
-    from AnkiVector.modules.helper_funcs.chat_status import user_admin
-    from AnkiVector.modules.sql import log_channel_sql as sql
+    from ChelseaRobot import EVENT_LOGS, LOGGER, dispatcher
+    from ChelseaRobot.modules.helper_funcs.chat_status import user_admin
+    from ChelseaRobot.modules.sql import log_channel_sql as sql
 
     def loggable(func):
         @wraps(func)

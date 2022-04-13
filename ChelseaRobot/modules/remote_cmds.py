8a15d1d06b3ab3e0b774a8300412727814b0d5c9
Xanon <74370153+Kinganonymous7762@users.noreply.github.com>
2022-04-13 15:36:15 +0530

    Update remote_cmds.py

--- ChelseaRobot/modules/remote_cmds.py
+++ ChelseaRobot/modules/remote_cmds.py
@@ -2,15 +2,15 @@ from telegram import ChatPermissions, Update
 from telegram.error import BadRequest
 from telegram.ext import CallbackContext, CommandHandler, run_async
 
-from AnkiVector import dispatcher
-from AnkiVector.modules.helper_funcs.chat_status import (
+from ChelseaRobot import dispatcher
+from ChelseaRobot.modules.helper_funcs.chat_status import (
     bot_admin,
     is_bot_admin,
     is_user_ban_protected,
     is_user_in_chat,
 )
-from AnkiVector.modules.helper_funcs.extraction import extract_user_and_text
-from AnkiVector.modules.helper_funcs.filters import CustomFilters
+from ChelseaRobot.modules.helper_funcs.extraction import extract_user_and_text
+from ChelseaRobot.modules.helper_funcs.filters import CustomFilters
 
 RBAN_ERRORS = {
     "User is an administrator of the chat",

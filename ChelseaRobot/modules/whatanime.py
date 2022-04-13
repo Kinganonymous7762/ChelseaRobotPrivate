2e88f0ae622d1dc0d72042143d7e09962fa4577c
Xanon <74370153+Kinganonymous7762@users.noreply.github.com>
2022-04-13 15:24:22 +0530

    Update whatanime.py

--- ChelseaRobot/modules/whatanime.py
+++ ChelseaRobot/modules/whatanime.py
@@ -12,7 +12,7 @@ import aiohttp
 from pyrogram import Client, filters
 from pyrogram.types import Message
 
-from AnkiVector import pbot
+from ChelseaRobot import pbot
 
 session = aiohttp.ClientSession()
 progress_callback_data = {}

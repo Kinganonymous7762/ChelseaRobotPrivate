d8dcf88502a1bdd881897982a047b299b539eaed
Xanon <74370153+Kinganonymous7762@users.noreply.github.com>
2022-04-13 15:30:06 +0530

    Update tagger.py

--- ChelseaRobot/modules/tagger.py
+++ ChelseaRobot/modules/tagger.py
@@ -5,11 +5,11 @@ from telegram.error import BadRequest
 from telegram.ext import CallbackQueryHandler, CommandHandler, Filters, run_async
 from telegram.utils.helpers import mention_html
 
-from AnkiVector import dispatcher
-from AnkiVector.modules.disable import DisableAbleCommandHandler
-from AnkiVector.modules.helper_funcs.alternate import typing_action
-from AnkiVector.modules.helper_funcs.chat_status import bot_admin, user_admin
-from AnkiVector.modules.helper_funcs.extraction import extract_user_and_text
+from ChelseaRobot import dispatcher
+from ChelseaRobot.modules.disable import DisableAbleCommandHandler
+from ChelseaRobot.modules.helper_funcs.alternate import typing_action
+from ChelseaRobot.modules.helper_funcs.chat_status import bot_admin, user_admin
+from ChelseaRobot.modules.helper_funcs.extraction import extract_user_and_text
 
 
 @run_async

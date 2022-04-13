6cb1ccac499d00ea6af5ef208ea33018e45b507a
Xanon <74370153+Kinganonymous7762@users.noreply.github.com>
2022-04-13 15:31:19 +0530

    Update stickers.py

--- ChelseaRobot/modules/stickers.py
+++ ChelseaRobot/modules/stickers.py
@@ -16,8 +16,8 @@ from telegram import (
 from telegram.ext import CallbackContext, run_async
 from telegram.utils.helpers import mention_html
 
-from AnkiVector import dispatcher
-from AnkiVector.modules.disable import DisableAbleCommandHandler
+from ChelseaRobot import dispatcher
+from ChelseaRobot.modules.disable import DisableAbleCommandHandler
 
 combot_stickers_url = "https://combot.org/telegram/stickers?q="
 

1c1c18d6e982bc3a6e9ae9aeb1b68aff1ea5febc
Xanon <74370153+Kinganonymous7762@users.noreply.github.com>
2022-04-13 15:28:38 +0530

    Update tts.py

--- ChelseaRobot/modules/tts.py
+++ ChelseaRobot/modules/tts.py
@@ -4,8 +4,8 @@ from gtts import gTTS
 from telegram import ChatAction, Update
 from telegram.ext import CallbackContext, run_async
 
-from AnkiVector import dispatcher
-from AnkiVector.modules.disable import DisableAbleCommandHandler
+from ChelseaRobot import dispatcher
+from ChelseaRobot.modules.disable import DisableAbleCommandHandler
 
 
 @run_async
